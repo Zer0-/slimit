fd5b138838163a400ea8e65e186c674c53c20c37
Philipp Volugine <phil.volguine@gmail.com>
2014-03-25 18:46:32 -0400

    lexer print statement and unicode_chars string literals changes
    For compatability with python3.4. Tested with 2.7 and 3.4 only

--- src/slimit/lexer.py
+++ src/slimit/lexer.py
@@ -22,6 +22,7 @@
 #
 ###############################################################################
 
+from __future__ import print_function
 __author__ = 'Ruslan Spivak <ruslan.spivak@gmail.com>'
 
 import ply.lex
@@ -432,6 +433,6 @@ class Lexer(object):
         return token
 
     def t_error(self, token):
-        print 'Illegal character %r at %s:%s after %s' % (
-            token.value[0], token.lineno, token.lexpos, self.prev_token)
+        print('Illegal character %r at %s:%s after %s' % (
+            token.value[0], token.lineno, token.lexpos, self.prev_token))
         token.lexer.skip(1)
